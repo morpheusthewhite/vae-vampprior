ab8a98c5a2b04cfa402ac384430a72157b807bc1
Vittorio <zampinetti@gmail.com>
2021-01-01 21:16:04 +0100

    added name param

--- vampprior/probabilities.py
+++ vampprior/probabilities.py
@@ -7,7 +7,7 @@ def log_bernoulli():
     pass
 
 
-def log_logistic256(x, mean, logvar, reduce_dim=None):
+def log_logistic256(x, mean, logvar, reduce_dim=None, name=None):
     """
     Discretized log-logistic. Similar to log-normal, but with heavier tails.
     @param reduce_dim: dimension of the data attributes, along which to sum the log-prob.
@@ -18,16 +18,16 @@ def log_logistic256(x, mean, logvar, reduce_dim=None):
     x_std = (tf.math.floor(x / binsize) * binsize - mean) / scale
     logp = tf.math.log(tf.sigmoid(x_std + binsize / scale) - tf.sigmoid(x_std) + 1e-7)
 
-    return tf.reduce_sum(logp, axis=reduce_dim)
+    return tf.reduce_sum(logp, axis=reduce_dim, name=name)
 
 
-def log_normal_standard(x, reduce_dim=None):
+def log_normal_standard(x, reduce_dim=None, name=None):
     log2pi = np.log(2 * np.pi)
     log_normal = -.5 * (log2pi + tf.math.pow(x, 2))
-    return tf.reduce_sum(log_normal, axis=reduce_dim)
+    return tf.reduce_sum(log_normal, axis=reduce_dim, name=name)
 
 
-def log_normal_diag(x, mean, logvar, reduce_dim=None):
+def log_normal_diag(x, mean, logvar, reduce_dim=None, name=None):
     """
     Multivariate log normal
     @param reduce_dim: dimension of the data attributes, along which to sum the log-prob.
@@ -35,4 +35,4 @@ def log_normal_diag(x, mean, logvar, reduce_dim=None):
     """
     log2pi = np.log(2 * np.pi)
     log_normal = -.5 * (log2pi + logvar + tf.math.pow(x - mean, 2) / tf.math.exp(logvar))
-    return tf.reduce_sum(log_normal, axis=reduce_dim)
+    return tf.reduce_sum(log_normal, axis=reduce_dim, name=name)
