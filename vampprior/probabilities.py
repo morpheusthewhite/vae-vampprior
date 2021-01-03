fa78fb082b9e616768dc7e82948b47d380d16b3a
morpheusthewhite <zffromGerace@hotmail.it>
2021-01-03 09:40:28 +0100

    fix probability functions docs

--- vampprior/probabilities.py
+++ vampprior/probabilities.py
@@ -12,6 +12,8 @@ def log_logistic256(x, mean, logvar, reduce_dim=None, name=None):
     Discretized log-logistic. Similar to log-normal, but with heavier tails.
     @param reduce_dim: dimension of the data attributes, along which to sum the log-prob.
         If tensor has shape (minibatch, sample_size) then provide reduce_dim=1
+        If tensor has shape (L, minibatch, sample_size) then provide reduce_dim=2
+        If not, rearrange the dimensions with tf.transpose to match one of the 2 cases
     """
     binsize = 1. / 256.
     scale = tf.math.exp(logvar)
@@ -22,6 +24,13 @@ def log_logistic256(x, mean, logvar, reduce_dim=None, name=None):
 
 
 def log_normal_standard(x, reduce_dim=None, name=None):
+    """
+    Multivariate log normal with standard parameters
+    @param reduce_dim: dimension of the data attributes, along which to sum the log-prob.
+        If tensor has shape (minibatch, sample_size) then provide reduce_dim=1
+        If tensor has shape (L, minibatch, sample_size) then provide reduce_dim=2
+        If not, rearrange the dimensions with tf.transpose to match one of the 2 cases
+    """
     log2pi = np.log(2 * np.pi)
     log_normal = -.5 * (log2pi + tf.math.pow(x, 2))
     return tf.reduce_sum(log_normal, axis=reduce_dim, name=name)
@@ -32,6 +41,8 @@ def log_normal_diag(x, mean, logvar, reduce_dim=None, name=None):
     Multivariate log normal
     @param reduce_dim: dimension of the data attributes, along which to sum the log-prob.
         If tensor has shape (minibatch, sample_size) then provide reduce_dim=1
+        If tensor has shape (L, minibatch, sample_size) then provide reduce_dim=2
+        If not, rearrange the dimensions with tf.transpose to match one of the 2 cases
     """
     log2pi = np.log(2 * np.pi)
     log_normal = -.5 * (log2pi + logvar + tf.math.pow(x - mean, 2) / tf.math.exp(logvar))
